172c621100ac1b59607b37c91e1162ef684e8d70
MerseniBilel <bilelmerseni7016@gmail.com>
2020-06-04 19:32:53 +0100

    layouts customization and job details are done git add .!

---
+++ resources/views/Jobs/jobDetails.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('content')
+<main>
+    <div class="job-post-company pt-120 pb-120">
+        <div class="container">
+            <div class="row justify-content-between">
+                <!-- Left Content -->
+                <div class="col-xl-7 col-lg-8">
+                    <!-- job single -->
+                    <div class="single-job-items mb-50">
+                        <div class="job-items">
+                            <div class="company-img company-img-details">
+                            <a href="#"><img src="{{$ownerDetails->er_pic}}" alt=""></a>
+                            </div>
+                            <div class="job-tittle">
+                                <h4 class="mt-5">{{$details->j_title}}</h4>
+                                <ul>
+                                    <li>{{$categoryDetails->cat_name}}</li>
+                                <li><i class="fas fa-map-marker-alt"></i>{{$details->j_location}}</li>
+                                <li>$3500 - ${{$details->j_salary}}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                      <!-- job single End -->
+
+                    <div class="job-post-details">
+                        <div class="post-details1 mb-50">
+                            <!-- Small Section Tittle -->
+                            <div class="small-section-tittle">
+                                <h4>Job Description</h4>
+                            </div>
+                        <p>{{$details->j_discription}}</p>
+                        </div>
+                        <div class="post-details2  mb-50">
+                             <!-- Small Section Tittle -->
+                            <div class="small-section-tittle">
+                                <h4>Required Knowledge, Skills, and Abilities</h4>
+                            </div>
+                           <ul>
+                               <li>System Software Development</li>
+                               <li>Mobile Applicationin iOS/Android/Tizen or other platform</li>
+                               <li>Research and code , libraries, APIs and frameworks</li>
+                               <li>Strong knowledge on software development life cycle</li>
+                               <li>Strong problem solving and debugging skills</li>
+                           </ul>
+                        </div>
+                        <div class="post-details2  mb-50">
+                             <!-- Small Section Tittle -->
+                            <div class="small-section-tittle">
+                                <h4>Education + Experience</h4>
+                            </div>
+                           <ul>
+                               <li>3 or more years of professional design experience</li>
+                               <li>Direct response email experience</li>
+                               <li>Ecommerce website design experience</li>
+                               <li>Familiarity with mobile and web apps preferred</li>
+                               <li>Experience using Invision a plus</li>
+                           </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Right Content -->
+                <div class="col-xl-4 col-lg-4">
+                    <div class="post-details3  mb-50">
+                        <!-- Small Section Tittle -->
+                       <div class="small-section-tittle">
+                           <h4>Job Overview</h4>
+                       </div>
+                      <ul>
+                      <li>Posted date : <span>{{$details->created_at}}</span></li>
+                          <li>Vacancy : <span>02</span></li>
+                          <li>Job nature : <span>Full time</span></li>
+                          <li>Salary :  <span>${{$details->j_salary}} monthly</span></li>
+                      </ul>
+                     <div class="apply-btn2">
+                        <a href="#" class="btn">Apply Now</a>
+                     </div>
+                   </div>
+                    <div class="post-details4  mb-50">
+                        <!-- Small Section Tittle -->
+                       <div class="small-section-tittle">
+                           <h4>Company Information</h4>
+                       </div>
+                    <span>{{$ownerDetails->er_company}}</span>
+                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        <ul>
+                            <li>Name: <span>{{$ownerDetails->er_company}} </span></li>
+                            <li>Phone : <span> {{$ownerDetails->er_phno}}</span></li>
+                            <li>Email: <span>{{$ownerDetails->email}}</span></li>
+                        </ul>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- job post company End -->
+
+</main>
+
+
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -108,7 +108,7 @@
                                 @enderror
                             </div>
                         </div>
-                        
+
                         <div class="form-group row">
                             <label for="ee_pic" class="col-md-4 col-form-label text-md-right">{{ __('Picture') }}</label>
 
@@ -147,7 +147,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="genric-btn danger-border circle">
                                     {{ __('Register') }}
                                 </button>
                             </div>
